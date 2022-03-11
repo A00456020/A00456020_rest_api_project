91ae4bb910350c26e337579f9f27fb4f1fab314c
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-11 19:34:47 -0400

    Updated model fields to not include blank values

--- rest_api_project/rest_api_app/models.py
+++ rest_api_project/rest_api_app/models.py
@@ -3,23 +3,23 @@ from django.db import models
 
 class Hotel(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=False)
-    country = models.CharField(max_length=100, null=False)
-    website = models.CharField(max_length=100, null=False)
-    city = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    country = models.CharField(max_length=100, null=False, blank=False)
+    website = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=100, null=True, blank=False)
 
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=False)
-    mobile = models.CharField(max_length=100, null=False)
-    email = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    mobile = models.CharField(max_length=100, null=False, blank=False)
+    email = models.CharField(max_length=100, null=False, blank=False)
 
 
 class Reservation(models.Model):
     id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
-    number_of_rooms = models.IntegerField(null=False, default=1)
-    checkin_date = models.DateField(null=False)
-    checkout_date = models.DateField(null=False)
+    number_of_rooms = models.IntegerField(null=False, default=1, blank=False)
+    checkin_date = models.DateField(null=False, blank=False)
+    checkout_date = models.DateField(null=False, blank=False)
