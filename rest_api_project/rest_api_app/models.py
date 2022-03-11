f4c50f8691d348bf0aa4be989dad7e41eb7f0c49
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-11 19:32:52 -0400

    Created Hotel, Customer and Reservation Models

--- rest_api_project/rest_api_app/models.py
+++ rest_api_project/rest_api_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    country = models.CharField(max_length=100, null=False)
+    website = models.CharField(max_length=100, null=False)
+    city = models.CharField(max_length=100, null=True)
+
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    mobile = models.CharField(max_length=100, null=False)
+    email = models.CharField(max_length=100, null=False)
+
+
+class Reservation(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    number_of_rooms = models.IntegerField(null=False, default=1)
+    checkin_date = models.DateField(null=False)
+    checkout_date = models.DateField(null=False)
