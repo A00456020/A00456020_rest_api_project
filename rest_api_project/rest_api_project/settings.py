9dabcd7c1fcab6ea208963486cab65244d04857e
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-11 18:59:08 -0400

    Updated settings.py to contain mysql database details

--- rest_api_project/rest_api_project/settings.py
+++ rest_api_project/rest_api_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rest_api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sql5478504',
+        'HOST': 'sql5.freemysqlhosting.net',
+        'USER': 'sql5478504',
+        'PASSWORD': '88v3kFz4Nz',
+        'PORT': '3306'
     }
 }
 
