6a3cb401fa5fa69e9f82043cb27f0a2aeed263a1
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-11 21:33:38 -0400

    Updated urls.py to include new URLs for customers and reservations views

--- rest_api_project/rest_api_app/urls.py
+++ rest_api_project/rest_api_app/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('hotels/', views.HotelView.as_view()),
-    path('hotels/<int:id>/', views.HotelView.as_view())
+    path('hotels/<int:id>/', views.HotelView.as_view()),
+    path('customers/', views.CustomerView.as_view()),
+    path('customers/<int:id>/', views.CustomerView.as_view()),
+    path('reservations/', views.ReservationView.as_view()),
+    path('reservations/<int:id>/', views.ReservationView.as_view())
 ]
