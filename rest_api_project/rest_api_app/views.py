35c64e017ca31d9e0fca63a43f4707f4486f0b76
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-11 21:34:27 -0400

    Created Customer and Reservation Views in views.py

--- rest_api_project/rest_api_app/views.py
+++ rest_api_project/rest_api_app/views.py
@@ -27,5 +27,48 @@ class HotelView(APIView):
             return Response(hotel_serializer.errors, status=400)
 
 
+class CustomerView(APIView):
+
+    def get(self, request, id=None):
+        if id:
+            queryset = Customer.objects.get(id=id)
+            query_serializer = CustomerSerializer(queryset)
+        else:
+            queryset = Customer.objects.all()
+            query_serializer = CustomerSerializer(queryset, many=True)
+        return Response(query_serializer.data)
+
+    def post(self, request):
+        customer_serializer = CustomerSerializer(data=request.data)
+        if customer_serializer.is_valid():
+            new_customer = customer_serializer.save()
+            query_serializer = CustomerSerializer(new_customer)
+            return Response(query_serializer.data, status=201)
+        else:
+            return Response(customer_serializer.errors, status=400)
+
+
+class ReservationView(APIView):
+
+    def get(self, request, id=None):
+        if id:
+            queryset = Reservation.objects.get(id=id)
+            query_serializer = ReservationSerializer(queryset)
+        else:
+            queryset = Reservation.objects.all()
+            query_serializer = ReservationSerializer(queryset, many=True)
+        return Response(query_serializer.data)
+
+    def post(self, request):
+        print(request.data)
+        reservation_serializer = ReservationSerializer(data=request.data)
+        if reservation_serializer.is_valid():
+            new_reservation = reservation_serializer.save()
+            query_serializer = ReservationSerializer(new_reservation)
+            return Response(query_serializer.data, status=201)
+        else:
+            return Response(reservation_serializer.errors, status=400)
+
+
 
 
